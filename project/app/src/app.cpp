cf27ab85ed0f88733502fe5f1546a2c29598b80e
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2022-12-02 00:03:09 +0100

    template cmake configuration with dummy files

--- project/app/src/app.cpp
+++ project/app/src/app.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <test.hpp>
+
 int main(int argc, char const *argv[])
 {
+    std::cout<<"Hello world !"<<std::endl;
+    test();
     return 0;
 }
---
+++ project/lib/presenter/src/test.cpp
@@ -0,0 +1,5 @@
+#include "../include/test.hpp"
+
+void test() {
+    std::cout<<"Hello from test !"<<std::endl;
+}
